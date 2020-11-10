5ed62ac21ee1bc3827ccac741caa919875178c1b
linusromland <romlandlinus@gmail.com>
2020-11-10 19:23:08 +0100

    Added basic Mongoose Functionality
    Co-Authored-By: Markussim <33492046+Markussim@users.noreply.github.com>

--- server/main.js
+++ server/main.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    mongoose = require('mongoose'),
+    app = express(),
+    port = 3000,
+    UserSchema = require("./models/UserSchema.js")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+    
+const clientDir = __dirname + "/client/";
+UserSchema.plugin(passportLocalMongoose)
+const User = mongoose.model('User',UserSchema);
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(clientDir));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
