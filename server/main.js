8919e6c1753f43cfc93bb72d48cafd5ad779a6f7
linusromland <romlandlinus@gmail.com>
2020-11-10 19:31:07 +0100

    added MongoDB Connect
    Co-Authored-By: Markussim <33492046+Markussim@users.noreply.github.com>

--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
+const dBModule = require('./dBModule.js')
 const express = require("express"),
     bodyParser = require("body-parser"),
     passportLocalMongoose = require("passport-local-mongoose"),
+    passport = require("passport"),
     mongoose = require('mongoose'),
     app = express(),
     port = 3000,
@@ -10,11 +12,20 @@ const express = require("express"),
 const clientDir = __dirname + "/client/";
 UserSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User',UserSchema);
+const UserDetails = mongoose.model('userInfo', User, 'userInfo');
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(clientDir));
 app.set("view engine", "ejs");
+
+dBModule.cnctDB("RomlandTubev2")
 
 app.get("/", (req, res) => {
     res.render("index");
