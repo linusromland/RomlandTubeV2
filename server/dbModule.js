c0bcefac720129eafe67dbbadd8cd41ea63eb006
linusromland <romlandlinus@gmail.com>
2020-11-10 20:09:53 +0100

    struff
    Co-Authored-By: Markussim <33492046+Markussim@users.noreply.github.com>

--- server/dbModule.js
+++ server/dbModule.js
@@ -1,41 +1,41 @@
 const mongoose = require('mongoose');
-const mongAuth = require('./mongoauth.json')
 let db;
 
-exports.cnctDBAuth = (collectionname) =>{
-  mongoose.connect(
-    "mongodb://localhost:27017/" + collectionname,
-    {
-      "auth": {
-        "authSource": "admin"
-      },
-      "user": mongAuth.username,
-      "pass": mongAuth.pass
-    }
-  );
+exports.cnctDBAuth = (collectionname) => {
+    const mongAuth = require('./mongoauth.json')
+    mongoose.connect(
+        "mongodb://localhost:27017/" + collectionname,
+        {
+            "auth": {
+                "authSource": "admin"
+            },
+            "user": mongAuth.username,
+            "pass": mongAuth.pass
+        }
+    );
 
-  db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function () {
-    console.log("logged in to " + collectionname)
-});
-
-}
-
-exports.cnctDB = (collectionname) =>{
-    let dbLink = `mongodb://localhost/${collectionname}`
-    mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true });
-  
     db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function () {
-      console.log("logged in to " + collectionname)
-  });
-  
-  }
+        console.log("logged in to " + collectionname)
+    });
+
+}
+
+exports.cnctDB = (collectionname) => {
+    let dbLink = `mongodb://localhost/${collectionname}`
+    mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+        console.log("logged in to " + collectionname)
+    });
+
+}
 
 exports.saveToDB = (input) => {
-    input.save(()=>{
-      console.log(`Successfully saved ${input} to the database!`)
+    input.save(() => {
+        console.log(`Successfully saved ${input} to the database!`)
     })
 }
--- server/main.js
+++ server/main.js
@@ -1,28 +1,23 @@
-const dBModule = require('./dBModule.js')
+const { session } = require("passport");
+
 const express = require("express"),
     bodyParser = require("body-parser"),
     passportLocalMongoose = require("passport-local-mongoose"),
     passport = require("passport"),
     mongoose = require('mongoose'),
+    expressSession = require('express-session'),
     app = express(),
     port = 3000,
-    UserSchema = require("./models/UserSchema.js")
+    UserSchema = require("./models/UserSchema.js"),
+    dBModule = require('./dbModule')
 
-    
+
 const clientDir = __dirname + "/client/";
-UserSchema.plugin(passportLocalMongoose)
-const User = mongoose.model('User',UserSchema);
-const UserDetails = mongoose.model('userInfo', User, 'userInfo');
-
-passport.use(UserDetails.createStrategy());
-
-passport.serializeUser(UserDetails.serializeUser());
-passport.deserializeUser(UserDetails.deserializeUser());
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(clientDir));
+
 app.set("view engine", "ejs");
 
 dBModule.cnctDB("RomlandTubev2")
